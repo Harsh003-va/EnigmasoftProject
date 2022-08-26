99be185aa298dafda7110494b4746600f6e37267
Manvitha Hegde <111779763+Manvithahegde190@users.noreply.github.com>
2022-08-26 17:06:22 +0530

    crud app

--- cafe_ui/src/App.js
+++ cafe_ui/src/App.js
@@ -4,8 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App"><h1>CRUD APPLICATION</h1>
-     
+
+    <div className="form">
+      <label>Movie Name:</label>
+     <input type="text"name="movieeName"/>
+     <label>Review:</label>
+     <input type="text"name="review" />
     
+    <button>Submit</button>
+    </div>
     </div>
   );
 }
