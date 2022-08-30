faab1095410912dfd8470f49438fc29a454937bb
Sriraj29 <srirajdesai29@gmail.com>
2022-08-30 16:04:58 +0530

    changes

--- cafe_ui/src/App.js
+++ cafe_ui/src/App.js
@@ -3,13 +3,21 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App"><h1>CRUD APPLICATION</h1>
+    <div className="App"><h1>Login</h1>
 
     <div className="form">
+<<<<<<< Updated upstream
       <label>Movie Name:</label>
      <input type="text"name="movieeName"/>
      <label>Review:</label>
      <input type="text"name="review" />
+=======
+      <label>Email:</label>
+     <input type="text"name="email"/>
+     <label>Password:</label>
+     <input type="text"name="password" />
+    
+>>>>>>> Stashed changes
     <button>Submit</button>
     </div>
     </div>
