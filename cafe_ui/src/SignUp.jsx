15c6382618cd6c1f692de4052bc75e3d1111315f
Manvitha Hegde <111779763+Manvithahegde190@users.noreply.github.com>
2022-09-15 10:58:16 +0530

    change
    change

--- cafe_ui/src/SignUp.jsx
+++ cafe_ui/src/SignUp.jsx
@@ -19,16 +19,12 @@ const SignUp = () => {
       <h1>Sign Up</h1>
       <h4>It's free and only takes a minute</h4>
       <form>
-        <label>First Name</label>
-        <input type="text" placeholder="" />
-        <label>Last Name</label>
+        <label>Name</label>
         <input type="text" placeholder="" />
         <label>Email</label>
         <input type="email" placeholder="" />
         <label>Password</label>
         <input type="password" placeholder="" />
-        <label>phone Number</label>
-        <input type="phone number" placeholder="" />
         <input type="button" value="Submit" />
       </form>
       <p class="para-2">
